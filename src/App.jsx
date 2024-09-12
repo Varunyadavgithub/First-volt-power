7b1bb2e83c03e7dffa118b9b36a5a1bcbe2c1417
Varun Yadav <vy056038@gmail.com>
2024-09-12 15:02:25 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import AboutUs from "./Pages/AboutUs";
+import Contact from "./Pages/Contact";
 
 const App = () => {
   return (
     <>
-      <h1 className='text-red-600'>hello</h1>
+      <div className="flex flex-col min-h-screen">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
