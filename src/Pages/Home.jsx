0b5f51a841e72969e3c4b31695cd0e091f39f412
Varun Yadav <vy056038@gmail.com>
2024-09-25 17:06:04 +0530

    Some changes in Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,15 +11,17 @@ import img_02 from "../assets/Images/service_02.jpg";
 import img_03 from "../assets/Images/service_03.jpg";
 import img_04 from "../assets/Images/service_04.jpg";
 import img_05 from "../assets/Images/service_05.jpg";
-import adani_logo from "../assets/Images/adani_Logo.png";
-import tata_logo from "../assets/Images/tata_power.png";
-import aditya_birla_group_logo from "../assets/Images/aditya_birla_group_logo.png";
-import kei_logo from "../assets/Images/kei_logo.png";
-import powerica_logo from "../assets/Images/powerica_logo.jpg";
-import chiripal_Logo from "../assets/Images/chiripal_Logo.png";
+import img_06 from "../assets/Images/service_06.jpg";
+import project_01 from "../assets/Images/project_01.jpg";
+import project_02 from "../assets/Images/project_02.jpg";
+import project_03 from "../assets/Images/project_03.jpg";
+import project_04 from "../assets/Images/project_04.jpg";
+import project_05 from "../assets/Images/project_05.jpg";
+import project_06 from "../assets/Images/project_06.jpg";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import ClientsSection from "../Components/ClientsSection";
 gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
@@ -64,16 +66,35 @@ const Home = () => {
         "Our rapid-response team is available 24/7 for emergency repairs, ensuring your operations continue with minimal downtime.",
       img: img_05,
     },
+    {
+      title: "Renewable Energy",
+      description:
+        "Installation of more than 1200 MW Solar Power Plant across India.",
+      img: img_06,
+    },
   ];
 
-  const clients = [
-    { name: "Adani", logo: adani_logo },
-    { name: "Tata Power", logo: tata_logo },
-    { name: "Aditya Birla Group", logo: aditya_birla_group_logo },
-    { name: "KEI", logo: kei_logo },
-    { name: "PowerICA", logo: powerica_logo },
-    { name: "Chiripal", logo: chiripal_Logo },
+  const projects = [
+    {
+      img: project_01,
+    },
+    {
+      img: project_02,
+    },
+    {
+      img: project_03,
+    },
+    {
+      img: project_04,
+    },
+    {
+      img: project_05,
+    },
+    {
+      img: project_06,
+    },
   ];
+ 
   useGSAP(() => {
     gsap.from(".welcom-text", {
       opacity: 0,
@@ -179,41 +200,42 @@ const Home = () => {
           <Link
             to="/about"
             onClick={scrollToTop}
-            className="bg-blue-900 hover:bg-blue-800 text-white py-3 px-6 rounded-lg font-semibold text-lg"
+            className="bg-gradient-to-r from-blue-500 to-indigo-500 hover:from-indigo-600 hover:to-blue-600 text-white py-3 px-6 rounded-lg font-semibold text-lg"
           >
             Learn More About Us
           </Link>
         </div>
       </section>
 
-      <section className="service py-16 px-4 bg-gray-200">
+      <section className="service py-16 px-4 bg-gradient-to-r from-blue-50 to-gray-100">
         <div className="container mx-auto text-center">
           <div className="text-center mb-12">
             <h1 className="text-4xl font-bold text-gray-800 mb-4">
               Our Services
             </h1>
-            <p className="text-lg text-gray-600">
+            <p className="text-lg text-gray-600 max-w-lg mx-auto">
               At <span className="font-semibold">First Volt Power</span>, we
               provide a comprehensive range of services to support all your
               electrical substation needs.
             </p>
           </div>
-          <div className="services-card grid md:grid-cols-3 gap-8">
+          <div className="grid md:grid-cols-3 gap-8">
             {services.map((service, index) => (
               <div
                 key={index}
-                className="bg-white shadow-lg rounded-lg hover:shadow-xl transform transition-transform duration-300 hover:scale-105 cursor-pointer"
+                className="bg-white shadow-lg rounded-lg hover:shadow-2xl transform transition-transform duration-300 hover:scale-105 cursor-pointer relative overflow-hidden"
               >
                 <img
                   src={service.img}
                   alt={service.title}
-                  className="mb-4 h-56 w-full rounded-t-lg"
+                  className="mb-4 h-56 w-full rounded-t-lg object-cover"
                 />
-                <div className="p-4">
+                <div className="absolute inset-0 bg-black bg-opacity-20 opacity-0 hover:opacity-100 transition-opacity duration-300"></div>
+                <div className="p-6">
                   <h3 className="text-2xl font-bold text-gray-800 mb-4">
                     {service.title}
                   </h3>
-                  <p className="text-gray-700">{service.description}</p>
+                  <p className="text-gray-700 mb-4">{service.description}</p>
                 </div>
               </div>
             ))}
@@ -222,7 +244,7 @@ const Home = () => {
             <Link
               to="/services"
               onClick={scrollToTop}
-              className="bg-blue-900 hover:bg-blue-800 text-white py-3 px-6 rounded-lg font-semibold text-lg"
+              className="bg-gradient-to-r from-blue-500 to-indigo-500 hover:from-indigo-600 hover:to-blue-600 text-white py-3 px-6 rounded-lg font-semibold text-lg shadow-lg transition-all hover:shadow-xl"
             >
               View All Services
             </Link>
@@ -230,19 +252,53 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="py-16 px-4 bg-white">
+     <ClientsSection/>
+
+      <section className="py-16 px-4 bg-gray-200 relative">
         <div className="container mx-auto text-center">
-          <h2 className="text-4xl font-bold text-gray-800 mb-6">Our Clients</h2>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-8 md:px-20">
-            {clients.map((client, index) => (
-              <div key={index} className="flex justify-center items-center">
+          <div className="text-center mb-12">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4 fadeInUp">
+              Our Projects
+            </h1>
+            <p className="text-lg text-gray-600 max-w-xl mx-auto fadeInUp delay-200">
+              Explore the remarkable projects we’ve successfully completed
+              across various industries, showcasing our commitment to excellence
+              in every detail.
+            </p>
+          </div>
+          <div className="grid md:grid-cols-3 gap-8 fadeInUp delay-400">
+            {projects.map((project, index) => (
+              <div
+                key={index}
+                className="relative group overflow-hidden rounded-lg shadow-lg hover:shadow-xl transform transition-all duration-500 hover:scale-105 cursor-pointer"
+              >
                 <img
-                  src={client.logo}
-                  alt={client.name}
-                  className="w-32 h-32 object-contain hover:scale-105 cursor-pointer"
+                  src={project.img}
+                  alt={`Project ${index + 1}`}
+                  className="w-full h-64 object-cover transition-transform duration-500 group-hover:scale-110"
                 />
+                {/* <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+                  <div className="text-center text-white p-4">
+                    <h3 className="text-2xl font-bold mb-2">
+                      Project Title {index + 1}
+                    </h3>
+                    <p className="text-sm">
+                      Click to learn more about this project and the solutions
+                      we provided.
+                    </p>
+                  </div>
+                </div> */}
               </div>
             ))}
+          </div>
+          <div className="mt-8">
+            <Link
+              to="/projects"
+              onClick={scrollToTop}
+              className="bg-gradient-to-r from-blue-600 to-blue-900 hover:from-blue-700 hover:to-blue-800 text-white py-3 px-6 rounded-lg font-semibold text-lg transition-all duration-300 transform hover:scale-105"
+            >
+              View All Projects
+            </Link>
           </div>
         </div>
       </section>
